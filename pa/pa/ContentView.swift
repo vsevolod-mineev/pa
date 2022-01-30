fa1ee1b39db9aa2ab1d33989ce4ee645d5e0ca16
Vsevolod Mineev <vsevolod.mineev@gmail.com>
2022-01-30 01:21:14 +0000

    init app

---
+++ pa/pa/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  pa
+//
+//  Created by vsevolod_mineev on 30/01/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ pa/pa/paApp.swift
@@ -0,0 +1,17 @@
+//
+//  paApp.swift
+//  pa
+//
+//  Created by vsevolod_mineev on 30/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct paApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
